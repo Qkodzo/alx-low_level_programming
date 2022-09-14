d487151f35cfe13bb49cc0fb82cb182a57d7f655
Prince Quaye <edemquaye6@gmail.com>
2022-09-13 17:26:24 -0700

    Adding 0-putchar.c to project 0x02 C

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,27 @@
-#include <unistd.h>
+#include "_putchar.h"
+#include "main.h"
 
 /**
- *_putchar - writes the character c to stdout
- *
- *@c: The character to print
+ *main -  prints _putchar, followed by a new line
  *
  *
  *
- *Return: On success 1.
+ *Return: Always 0 (Success)
  *
- *On error, -1 is returned, and errno is set appropriately.
+ *
  *
  */
 
-int _putchar(char c)
+int main(void)
 {
-
-	return (write(1, &c, 1));
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
+		return (0);
 }
