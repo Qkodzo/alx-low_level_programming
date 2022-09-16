e1ebf5f3e27f925dcd40609d5d0001ff6a5bea2e
Prince Quaye <edemquaye6@gmail.com>
2022-09-16 16:28:16 -0700

    Adding 0-main.c to 0x03 C

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,25 +1,18 @@
 #include "main.h"
 
 /**
- *main - assigns a random number to int n everytime
+ *main - tests function that prints if integer is positive or negative
  *
- *it executes, and prints it
- *
- *Return: Always 0 (Success)
+ *Return: 0
  *
  */
 
-void positive_or_negative(int i)
+int main(void)
 {
-		int n;
+		int i;
 
-		srand(time(0));
-		n = rand() - RAND_MAX / 2;
-		if (n > 0)
-			printf("%d is positive\n", n);
-		else if (n == 0)
-			printf("%d is zero\n", n);
-		else if (n < 0)
-			printf("%d is negative\n", n);
+		i = 0;
+		positive_or_negative(i);
+
 		return (0);
 }
